f44b116f74d39f4a1898c08926b237c632e84ab4
Javier Moreno <mq2880@gmail.com>
2020-11-27 12:16:20 +0100

    Commit

---
+++ Cupcakes/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Cupcakes.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cupcakes
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CupcakeContext>(options => options.UseSqlite("Data Source=cupcake.db"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, CupcakeContext cupcakeContext)
+        {
+            cupcakeContext.Database.EnsureDeleted();
+            cupcakeContext.Database.EnsureCreated();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "CupcakeRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Cupcake", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 20846C-Mod7-Lab-Exercise1
+# Module 7: Using Entity Framework Core in ASP.NET Core
+
+## Lab: Using Entity Framework Core in ASP.NET Core
+
+1. **Nombres y apellidos:** Francisco Javier Moreno Quevedo
+2. **Fecha:** 27/11/2020
+3. **Resumen del Ejercicio:** 
+4. **Dificultad o problemas presentados y como se resolvieron:** Ninguna
+
+
+
+- Ejercicio 1: Adding Entity Framework Core 
+
+  - Creamos la clase **Cupcake** y **Bakery** en el modelo
+  - Añadimos propiedades y las decoramos para su visualización
+  - Creamos la clase CupcakeContext que hereda de DbContext
+  - Configuramos el proyecto para que pueda usar SQLLite.
+  - Configuramos el Middleware
+  - Creamos el metodo **OnModelCreating** que poblará de datos las clases que hemos creado
